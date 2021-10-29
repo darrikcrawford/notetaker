31bbcdab67d5b5b42c3bcbedbff839a506f5ddac
Darrik Crawford <darrikcrawford@yahoo.com>
2021-10-28 22:16:15 -0500

    add post route and middle wear

--- server.js
+++ server.js
@@ -5,8 +5,25 @@ const { notes } = require('./Develop/data/notes.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+function createNewNote(body, notesArray) {
+  console.log(body);
+  // our function's main code will go here!
+
+  // return finished code to post route for response
+  return body;
+}
+
 app.get('/api/notes', (req, res) => {
   res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  res.json(req.body);
 });
 
 app.listen(3001, () => {
